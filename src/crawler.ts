e0be91c376c2cf5437da56fbedc31503996c6586
Mehmet Kaan Aksoy <mehmetkaanaksoy13@gmail.coms>
2024-11-12 23:16:35 +0300

    Added getURLsFromHTML function and some tests

--- src/crawler.ts
+++ src/crawler.ts
@@ -1,16 +1,44 @@
 "use strict";
 
-function normalizeURL(url: string): string {
-    const urlObj: URL = new URL(url);
-    const path =  (urlObj.host + urlObj.pathname).toLowerCase();
-    
-    if (path.length > 0 && path.slice(-1) === '/') {
-        return path.slice(0, -1)
+function getURLsFromHTML(body: string, baseUrl: string): string[] {
+  const urlMatches: RegExpExecArray[] = Array.from(
+    body.matchAll(/href="([^"]+)"/g)
+  );
+  const links: string[] = Array.from(urlMatches, (match) => match[1]);
+  const urls: string[] = [];
+
+  for (const link of links) {
+    if (link.slice(0, 1) === "/") {
+      // Relative links
+      try {
+        const urlObj: URL = new URL(baseUrl + link);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.error(`Invalid relative link: ${link}, error: ${err}`);
+      }
     } else {
-        return path;
+      // Absolute links
+      try {
+        const urlObj: URL = new URL(link);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.error(`Invalid absolute link: ${link}, error: ${err}`);
+      }
     }
+  }
+
+  return urls;
 }
 
-export {
-    normalizeURL
+function normalizeURL(url: string): string {
+  const urlObj: URL = new URL(url);
+  const path: string = (urlObj.host + urlObj.pathname).toLowerCase();
+
+  if (path.length > 0 && path.slice(-1) === "/") {
+    return path.slice(0, -1);
+  } else {
+    return path;
+  }
 }
+
+export { normalizeURL, getURLsFromHTML };
--- tests/crawler.test.ts
+++ tests/crawler.test.ts
@@ -1,36 +1,101 @@
 "use strict";
 
-import { assertEquals } from "jsr:@std/assert";
-import { normalizeURL } from "../src/crawler.ts";
+import { normalizeURL, getURLsFromHTML } from "../src/crawler.ts";
+import { assertEquals } from "https://deno.land/std@0.224.0/testing/asserts.ts";
 
 Deno.test("normalizeURL strip protocol", () => {
-  const input = "https://test.example.com/path"
-  const actual = normalizeURL(input)
-  const expected = "test.example.com/path"
+  const input = "https://test.example.com/path";
 
+  const actual = normalizeURL(input);
+  const expected = "test.example.com/path";
+
+  
   assertEquals(actual, expected);
 });
 
 Deno.test("normalizeURL strip trailing slash", () => {
-  const input = "https://test.example.com/path/"
-  const actual = normalizeURL(input)
-  const expected = "test.example.com/path"
+  const input = "https://test.example.com/path/";
+  const actual = normalizeURL(input);
+  const expected = "test.example.com/path";
 
   assertEquals(actual, expected);
 });
 
 Deno.test("normalizeURL capitals", () => {
-  const input = "HTTPS://TEST.EXAMPLE.COM/PATH"
-  const actual = normalizeURL(input)
-  const expected = "test.example.com/path"
+  const input = "HTTPS://TEST.EXAMPLE.COM/PATH";
+  const actual = normalizeURL(input);
+  const expected = "test.example.com/path";
 
   assertEquals(actual, expected);
 });
 
 Deno.test("normalizeURL strip http", () => {
-  const input = "http://test.example.com/path"
-  const actual = normalizeURL(input)
-  const expected = "test.example.com/path"
+  const input = "http://test.example.com/path";
+  const actual = normalizeURL(input);
+  const expected = "test.example.com/path";
+
+  assertEquals(actual, expected);
+});
+
+Deno.test("getURLsFromHTML absolute", () => {
+  const HTMLBody = `
+    <html>
+      <body>
+        <a href="https://example.com/">Example 1</a>
+        <a href="https://test1.example.com/">Example 2</a>
+      </body>
+    </html>
+  `;
+  const input = "https://example.com/";
+  const actual = getURLsFromHTML(HTMLBody, input);
+  const expected: string[] = ["https://example.com/", "https://test1.example.com/"];
+
+  assertEquals(actual, expected);
+});
+
+Deno.test("getURLsFromHTML relative", () => {
+  const HTMLBody = `
+    <html>
+      <body>
+        <a href="/path1/">Example 1</a>
+        <a href="/path2/">Example 2</a>
+      </body>
+    </html>
+  `;
+  const input = "https://example.com";
+  const actual = getURLsFromHTML(HTMLBody, input);
+  const expected: string[] = ["https://example.com/path1/", "https://example.com/path2/"];
+
+  assertEquals(actual, expected);
+});
+
+Deno.test("getURLsFromHTML both", () => {
+  const HTMLBody = `
+    <html>
+      <body>
+        <a href="https://example.com/path/">Example 1</a>
+        <a href="/path/">Example 2</a>
+      </body>
+    </html>
+  `;
+  const input = "https://example.com";
+  const actual = getURLsFromHTML(HTMLBody, input);
+  const expected: string[] = ["https://example.com/path/", "https://example.com/path/"];
+
+  assertEquals(actual, expected);
+});
+
+Deno.test("getURLsFromHTML invalid", () => {
+  const HTMLBody = `
+    <html>
+      <body>
+        <a href="invalid">Invalid URL</a>
+      </body>
+    </html>
+  `;
+  const input = "https://example.com/";
+  const actual = getURLsFromHTML(HTMLBody, input);
+  const expected: string[] = [];
 
   assertEquals(actual, expected);
 });
