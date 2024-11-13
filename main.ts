15e175cb3100d16f1b81c69727c2780953b5f003
Mehmet Kaan Aksoy <mehmetkaanaksoy13@gmail.coms>
2024-11-13 16:58:07 +0300

    Optimized code

--- main.ts
+++ main.ts
@@ -4,18 +4,16 @@ import { crawl } from "./src/crawler.ts";
 import { report } from "./src/report.ts";
 
 async function main() {
-  const count = Deno.args.length;
-
-  if (count <= 0) {
+  if (Deno.args.length <= 0) {
     console.log("No websites provided.");
     Deno.exit(1);
-  } else {
-    for (const [index, url] of Deno.args.entries()) {
-      console.log(`Starting crawl of website ${index + 1}: ${url}...`);
-      const pages = await crawl({ baseUrl: url, url, pages: {} });
+  }
 
-      report({ pages });
-    }
+  for (const [index, url] of Deno.args.entries()) {
+    console.log(`Starting crawl of website ${index + 1}: ${url}...`);
+    const pages = await crawl({ baseUrl: url, url, pages: {} });
+
+    report({ pages });
   }
 }
 
